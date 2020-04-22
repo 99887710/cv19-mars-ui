e944604462d1887e75f16414bde1911745977bbb
jeff wu <j98728@hotmail.com>
2020-04-22 05:59:34 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 // app.use('/', express.static(path.join(__dirname, 'public')))
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, 'public')));
+ㄥapp.use(express.static(path.join(__dirname, 'public')));
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
