beb8271563272b23a492dd41fd044ba11ae7b2e4
jeff wu <j98728@hotmail.com>
2020-04-22 05:48:59 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+// app.use('/', express.static(path.join(__dirname, 'public')))
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'public')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log("Listening on Port", port)) 
